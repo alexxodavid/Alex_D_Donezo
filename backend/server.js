ffaab1cad6724eec21f24acc12a56d944ad2cffb
Alex Odysseus David <alexxodavid@gmail.com>
2025-07-14 18:32:48 -0400

    Getting set up and the backend files done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use("/todos", verifyToken, todoRouter);
+
+app.use("/todos", todoRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 });
+
+import todoRouter from "./routes/todo.js";
+
+import verifyToken from "./middleware/auth.js";
